98e5b7f4c550f020778b2df8c3d1a6d5f2c782a9
Ryan Sibbaluca <ryanral@sg.ibm.com>
2017-01-24 17:38:58 +0800

    Update header and content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
 console.log('\'Allo \'Allo!');
+
+$(window).scroll(function() {
+  var scrollFromTop = $(this).scrollTop();
+
+  $('.logo').css({
+    'transform': 'translate(0px, '+ scrollFromTop/3 +'%)'
+  });
+
+  $('.logo-name').css({
+    'transform': 'translate(0px, -' + scrollFromTop/10 + '%)'
+  })
+});
