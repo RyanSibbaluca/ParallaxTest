1a2467f8a58e83e446dffa7603e20a38b621d390
Ryan Sibbaluca <ryanral@sg.ibm.com>
2017-04-25 16:45:58 +0800

    Improve colors, animation, icons, meta tag

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+var firstSectionTop = $('section#summary').offset().top;
+
 $(window).scroll(function() {
   var scrollFromTop = $(this).scrollTop();
 
@@ -8,4 +10,34 @@ $(window).scroll(function() {
   $('.logo-name').css({
     'transform': 'translate(0px, -' + scrollFromTop/10 + '%)'
   });
+
+  if (scrollFromTop >= firstSectionTop) {        // If page is scrolled more than 50px
+    $('#return-to-top').fadeIn(300);    // Fade in the arrow
+  } else {
+    $('#return-to-top').fadeOut(300);   // Else fade out the arrow
+  }
+});
+
+$('#return-to-top').click(function() {      // When arrow is clicked
+  $('body,html').animate({
+    scrollTop : 0                       // Scroll to top of body
+  }, 500);
+});
+
+
+$('#scroll-to-first-section').click(function() {
+  $('body,html').animate({
+    scrollTop : firstSectionTop                     // Scroll to first section
+  }, 1000);
+});
+
+var $root = $('html, body');
+$('a').click(function() {
+  var href = $.attr(this, 'href');
+  $root.animate({
+    scrollTop: $(href).offset().top
+  }, 1000, function () {
+    window.location.hash = href;
+  });
+  return false;
 });
