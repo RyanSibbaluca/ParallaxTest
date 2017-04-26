fde429400e90378cf7d305af92004add37cbeae8
Ryan Sibbaluca <ryanral@sg.ibm.com>
2017-04-26 16:47:41 +0800

    Add animations and scrollingmagic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,9 @@
 var firstSectionTop = $('section#summary').offset().top;
+var sectionHeader = $('section#summary h1:first-child');
 
 $(window).scroll(function() {
   var scrollFromTop = $(this).scrollTop();
+  // console.log($(this).scroll)
 
   $('.logo').css({
     'transform': 'translate(0px, '+ scrollFromTop/3 +'%)'
@@ -11,11 +13,41 @@ $(window).scroll(function() {
     'transform': 'translate(0px, -' + scrollFromTop/10 + '%)'
   });
 
-  if (scrollFromTop >= firstSectionTop) {        // If page is scrolled more than 50px
-    $('#return-to-top').fadeIn(300);    // Fade in the arrow
+  //fade the scroll to top and page navigation
+  //within the header
+  if (scrollFromTop >= firstSectionTop) {
+    $('#return-to-top').fadeIn(300);
+    // $('#page-nav').fadeIn(300);
   } else {
-    $('#return-to-top').fadeOut(300);   // Else fade out the arrow
+    $('#return-to-top').fadeOut(300);
+    // $('#page-nav').fadeOut(300);
   }
+
+  var pageNavPosition = $('#page-nav').offset().top;
+
+
+  if(scrollFromTop - firstSectionTop > 200) {
+    var offset = scrollFromTop - firstSectionTop;
+    console.log(offset);
+    var offsetX = Math.min(0, offset/3 - 100);
+    $('section#summary h1:first-child').css({
+      'transform': 'translate('+ offsetX +'px, 0px)'
+    });
+  }
+
+  // if(pageNavPosition >= sectionHeader.next().offset().top ) {
+  //
+  //   var offset = pageNavPosition - sectionHeader.next().offset().top;
+  //
+  //   var offsetX = Math.min(0, offset - 100);
+  //   var offsetY = Math.max(0, -1 * (offset - 80));
+  //   var letterSpacing =  Math.min(0, Math.round(offset / 100 * 15 - 15));
+  //   $('section#summary h1:first-child').css({
+  //     'transform': 'translate('+ offsetX +'px, ' + offsetY + 'px)',
+  //     'letter-spacing': letterSpacing + 'px'
+  //   });
+  // }
+
 });
 
 $('#return-to-top').click(function() {      // When arrow is clicked
